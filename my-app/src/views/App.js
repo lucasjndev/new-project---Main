827a0b58088f6f80a22c76a9ea70dd2d1138dfc0
lucasjesusn <lucasjnascimento.dev@gmail.com>
2022-04-18 12:57:05 -0300

    nova mensagem

--- my-app/src/views/App.js
+++ my-app/src/views/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" />
-        Sabor Quentinho! :D
+        Sabor Quentinho! :D  ...
       </header>
     </div>
   );
