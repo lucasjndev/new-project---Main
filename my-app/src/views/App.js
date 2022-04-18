d3ac20957a8f63e67095eb2ae4d2356d11a79919
lucasjesusn <lucasjnascimento1@gmail.com>
2022-04-18 01:44:35 -0300

    new page

---
+++ my-app/src/views/App.js
@@ -0,0 +1,15 @@
+import logo from './../img/Hot-Cup-Of-Coffee.svg';
+import './../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" >
+        <img src={logo} className="App-logo" alt="logo" />
+        Sabor Quentinho
+      </header>
+    </div>
+  );
+}
+
+export default App;
