36babd0588fc1e6a6741c66a03b81d39aeb213f5
lucasjesusn <lucasjnascimento.dev@gmail.com>
2022-04-18 11:49:13 -0300

    ok ok

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,9 @@ import App from './views/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-
     <App />
-
   </React.StrictMode>
 );
 
